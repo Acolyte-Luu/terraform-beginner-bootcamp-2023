ea188f0e7e70597b4780fd4cf38796f76b66f6e7
Kwadwo Addo <kwadwoaddo97@gmail.com>
2023-10-10 06:04:10 +0000

    #48 fix provider to allow more than 1 terra home to be created

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:nzd4/2vDRt3LHzETEonvtbi/bWWYE2laYXfE252Vm1o=",
+    "h1:x3UXUgyHWneIpwjuUm6ybE2Ugr5qun5dWjZ//bzCk+w=",
   ]
 }
 
--- variables.tf
+++ variables.tf
@@ -6,21 +6,31 @@ variable "teacherseat_user_uuid" {
 #    type = string
 #}
 
-variable "index_html_filepath" {
-    type = string
+#variable "index_html_filepath" {
+#    type = string
+#}
+
+#variable "error_html_filepath" {
+#    type = string
+#}
+
+variable "bloodborne" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "error_html_filepath" {
-    type = string
+variable "elden_ring" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "content_version" {
-  type = number
-}
-
-variable "assets_path" {
-  type = string
-}
+#variable "assets_path" {
+#  type = string
+#}
 
 variable "terratowns_endpoint" {
     type = string 
